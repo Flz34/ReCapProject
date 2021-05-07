90f076488baa473b84e576cadb3f7dcb0f811e6b
FİLİZ KAHRIMAN <FILIZKAHRIMAN@DESKTOP-RAMVN4B>
2021-05-07 13:30:40 +0300

    7. Gün Ödev
    Araç Kiralama Sistemi Başlangıç

---
+++ ReCapProject/ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using System;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.ModelYear);
+            }
+
+            
+        }
+    }
+}
---
+++ ReCapProject/Entities/Concrete/Car.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //implement edilmemiş olması ileride sıkıntı oluşturucak
+    public class Car:IEntity
+    {
+        public int Id { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public string ModelYear { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string Description { get; set; }
+
+    }
+}
